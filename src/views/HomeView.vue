b2948ddb33edf585fa0fd00f3f7ad035a87d25ff
TeodorPetrovic <teodor.z.petrovic@gmail.com>
2024-11-15 08:27:32 +0100

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+const tasks = ref();
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+onMounted(async () => {
+  const response = await axios.get(`${API_BASE_URL}/tasks`);
+  tasks.value = response.data;
+})
+
+</script>
+
+<template>
+  <h1>Tasks</h1>
+  <ul>
+    <li v-for="task in tasks" :key="task.id">{{ task.title }}: {{ task.description }}</li>
+  </ul>
+</template>
